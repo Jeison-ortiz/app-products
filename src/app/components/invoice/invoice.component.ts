9b221ceed1fc4baab2578ea3c00e08f837465fd8
Jeison Ortiz <ortizjeison4@gmail.com>
2024-05-01 17:54:25 -0500

    feat: Se agrega la opción de eliminar componentes

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -5,13 +5,17 @@ import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
 import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
+import { TotalComponent } from '../total/total.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent],
-  templateUrl: './invoice.component.html',
-  styleUrl: './invoice.component.css'
+  imports: [InvoiceViewComponent,
+     ClientViewComponent,
+     CompanyViewComponent,
+     ListItemsComponent,
+    TotalComponent],
+  templateUrl: './invoice.component.html'
 })
 export class InvoiceComponent implements OnInit{
   invoice!: Invoice;
@@ -21,5 +25,8 @@ export class InvoiceComponent implements OnInit{
     this.invoice = this.service.getInvoice();
   }
 
+  removeItem(id: number){
+    this.invoice = this.service.remove(id);
+  }
 
 }
