234e28dd965fc11bca0dd84e4e41cba98cab28fe
Jeison Ortiz <ortizjeison4@gmail.com>
2024-05-01 11:14:58 -0500

    feat: se crea el service para obtener la data simulada.

---
+++ src/app/services/invoice.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Invoice} from '../models/invoice';
+import { invoiceData } from '../data/invoice.data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  private invoice: Invoice = invoiceData;
+  constructor() { }
+
+  getInvoice(): Invoice{
+    const total = this.calculateTotal();
+    return {... this.invoice, total:total};
+  }
+
+  calculateTotal(){
+  //  let total = 0;
+  //  this.invoice.items.forEach(item => {
+  //    total += item.total();
+  //  })
+  //  return total;
+    return this.invoice.items.reduce((total, item)=> total + item.total(),0)
+  }
+}
